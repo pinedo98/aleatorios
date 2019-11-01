9c45cea608935bc7fa864bf717acf4fd775793bb
Fernando Pinedo <fernando.pinedo@outlook.com>
2019-11-01 02:11:45 -0600

    Polish algorithms

--- utils/algorithms.js
+++ utils/algorithms.js
@@ -1,11 +1,12 @@
 export function generateWithCongruencial(seed, k, mod, add) {
-  let constant = add + (8 * k)
+  let constant = Number(add) + (8 * k)
+  
   let numbers = [];
   while(numbers.length < 1001) {
     let xi = (constant * seed) % mod;
     let ri = xi / (mod - 1);
-    if ((numbers.indexOf(ri) != -1)) break;
-    numbers.push(ri);
+    if ((numbers.includes(Number("." + ri)))) break;
+    numbers.push(Number("." + ri));
     seed = xi;
   }
   return numbers;
@@ -13,7 +14,6 @@ export function generateWithCongruencial(seed, k, mod, add) {
 
 export function generateWithCuadrados(seed) {
   let digits = seed.toString().length;
-  console.log("Cantidad de digitos:  " + digits);
   let x1 = seed;
   let numbers = [];
 
@@ -21,9 +21,9 @@ export function generateWithCuadrados(seed) {
     let y1 = Math.pow(x1, 2);
     let y1Digits = y1.toString().length;
     let temp = (y1Digits - digits) / 2;
-    x2 = y1.toString().substring(temp, temp + digits);
-    if ((numbers.indexOf(x2) != -1)) break;
-    numbers.push(x2);
+    let x2 = y1.toString().substring(temp, temp + digits);
+    if ((numbers.includes(Number("." + x2)))) break;
+    numbers.push(Number("." + x2));
     x1 = x2;
   }
   return numbers;
@@ -40,8 +40,8 @@ export function generateWithMultiplicador(seed, a) {
     let y0Digits = y0.toString().length;
     let temp = (y0Digits - digits) / 2;
     x1 = y0.toString().substring(temp, temp + digits);
-    if ((numbers.indexOf(x1) != -1)) break;
-    numbers.push(x1);
+    if ((numbers.includes(Number("." + x1)))) break;
+    numbers.push(Number("." + x1));
     seed = x1;
   } 
   return numbers;
