64e0f9ccb05b2a66c576e10b59e9e47fb5d33687
Fernando Pinedo <fernando.pinedo@outlook.com>
2019-11-01 02:10:21 -0600

    Finish algorithms UI

--- components/Cuadrados.jsx
+++ components/Cuadrados.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Input from './Input'
-import {generateWithCongruencial} from '../utils/algorithms'
+import {generateWithCuadrados} from '../utils/algorithms'
 
-export default class Congruencial extends React.Component {
+export default class Cuadrados extends React.Component {
 	state = {}
 
 	generate() {
-		let numbers = generateWithCongruencial(this.state.seed, this.state.constant, this.state.mod, this.state.add);
+		let numbers = generateWithCuadrados(this.state.seed);
 		let mediaTest = false;
 		let varianzaTest = false;
 		let independenciaTest = false;
@@ -26,9 +26,6 @@ export default class Congruencial extends React.Component {
 			<section className='container'>
 				<div>
 					<Input label='Semilla' onChange={e => this.setState({ seed: e.target.value }) }/>
-					<Input label='Constante Multiplicativa K' onChange={e => this.setState({ constant: e.target.value }) }/>
-					<Input label='A sumar' onChange={e => this.setState({ add: e.target.value }) }/>
-					<Input label='Módulo' onChange={e => this.setState({ mod: e.target.value }) }/>
 
 					<button onClick={() => this.generate()}> Generar </button>
 				</div>
--- components/Multiplicador.jsx
+++ components/Multiplicador.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import Input from './Input'
-import {generateWithCongruencial} from '../utils/algorithms'
+import { generateWithMultiplicador } from "../utils/algorithms";
 
-export default class Congruencial extends React.Component {
+export default class Multiplicador extends React.Component {
 	state = {}
 
 	generate() {
-		let numbers = generateWithCongruencial(this.state.seed, this.state.constant, this.state.mod, this.state.add);
+		let numbers = generateWithMultiplicador(this.state.seed, this.state.a);
 		let mediaTest = false;
 		let varianzaTest = false;
 		let independenciaTest = false;
@@ -26,9 +26,7 @@ export default class Congruencial extends React.Component {
 			<section className='container'>
 				<div>
 					<Input label='Semilla' onChange={e => this.setState({ seed: e.target.value }) }/>
-					<Input label='Constante Multiplicativa K' onChange={e => this.setState({ constant: e.target.value }) }/>
-					<Input label='A sumar' onChange={e => this.setState({ add: e.target.value }) }/>
-					<Input label='Módulo' onChange={e => this.setState({ mod: e.target.value }) }/>
+					<Input label='Constante Multiplicativa a' onChange={e => this.setState({ a: e.target.value }) }/>
 
 					<button onClick={() => this.generate()}> Generar </button>
 				</div>
